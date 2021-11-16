76568a188ad6851e7fad8040629a8c9fac8bc3a7
RodrigoSouzaSilva <rodrigo_souza.91@hotmail.com>
2021-11-16 12:15:51 -0300

    criando unidade 1

---
+++ 01_Introducao_node.js/app.js
@@ -0,0 +1,17 @@
+const { soma, sub, multi, divi } = require("./calculadora");
+
+soma(4, 8)
+sub(4, 9)
+multi(4, 9)
+divi(3, 90)
+
+// let mostrarSite = true
+// const site = 'www.guiadoprogramador.com'
+
+// console.log('Hello World!!!')
+// console.log('Meu nome é Rodrigo de Souza');
+// console.log('Estou aprendendo Node.js');
+
+// if(mostrarSite) {
+//     console.log(site);
+// }
---
+++ 01_Introducao_node.js/calculadora.js
@@ -0,0 +1,17 @@
+function soma(a, b){
+    return console.log(a + b);
+}
+
+function sub(a, b){
+    return console.log(a - b);
+}
+
+function multi(a, b){
+    return console.log(a * b);
+}
+
+function divi(a, b){
+    return console.log(a / b);
+}
+
+module.exports = { soma, sub, multi, divi }
