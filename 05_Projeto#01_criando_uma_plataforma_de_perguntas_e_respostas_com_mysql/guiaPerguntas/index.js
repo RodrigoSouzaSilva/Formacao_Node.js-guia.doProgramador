d2766a2b7dd54de491ae003c85dd95b5e3990ed0
RodrigoSouzaSilva <rodrigo_souza.91@hotmail.com>
2021-12-02 08:06:12 -0300

    atualizando anotações e projeto

--- 05_Projeto#01_criando_uma_plataforma_de_perguntas_e_respostas_com_mysql/guiaPerguntas/index.js
+++ 05_Projeto#01_criando_uma_plataforma_de_perguntas_e_respostas_com_mysql/guiaPerguntas/index.js
@@ -4,19 +4,37 @@ const app = express()
 // Dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Olá você está na página principal do projeto')
+})
+
 app.get('/:nome/:lang', (req, res) => {
     let nome = req.params.nome
     let lang = req.params.lang
     let exibirMsg = false
+
+
+    let produtos = [
+        {nome: 'Doritos', preco: 3.14},
+        {nome: 'Coca-Cola', preco: 5},
+        {nome: 'Leite', preco: 1.45},
+        {nome: 'Carne', preco: 15},
+        {nome: 'Red-Bull', preco: 6}
+    ]
+
+
     res.render('index', {
         nome: nome,
         lang: lang,
         empresa: 'Guia do programador',
         inscrito: 8000,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     })
 })
 
 app.listen(8080, () => {
-    console.log('App rodando');
+    console.log('App rodando na porta http://localhost:8080/');
 })
