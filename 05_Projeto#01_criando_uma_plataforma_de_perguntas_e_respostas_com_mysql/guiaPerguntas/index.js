f3c631936907f03f1483c603c5fafda824768040
RodrigoSouzaSilva <rodrigo_souza.91@hotmail.com>
2021-12-04 07:44:19 -0300

    atualizando projeto e anotações 05

--- 05_Projeto#01_criando_uma_plataforma_de_perguntas_e_respostas_com_mysql/guiaPerguntas/index.js
+++ 05_Projeto#01_criando_uma_plataforma_de_perguntas_e_respostas_com_mysql/guiaPerguntas/index.js
@@ -54,8 +54,14 @@ app.get('/pergunta/:id',(req, res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined){
-            res.render('pergunta', {
-                pergunta: pergunta
+            Resposta.findAll({
+                where:{perguntaId: pergunta.id},
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render('pergunta', {
+                    pergunta: pergunta,
+                    respostas: respostas
+                })
             })
         } else {
             res.redirect('/')
