2a3690f5c994fd9789ff9139a9cdbe7bc22e7a1c
RodrigoSouzaSilva <rodrigo_souza.91@hotmail.com>
2021-12-28 20:10:14 -0300

    atualizando projeto eanotações 07

--- 07_Projeto#02_Criando_um_blog_com_painel_administrativo/guiaPress/categories/CategoriesController.js
+++ 07_Projeto#02_Criando_um_blog_com_painel_administrativo/guiaPress/categories/CategoriesController.js
@@ -48,4 +48,22 @@ router.post('/categories/delete', (req, res) => {
     }
 })
 
+router.get('/admin/categories/edit/:id', (req, res) => {
+    let id = req.params.id
+
+    if(isNaN(id)){
+        res.redirect('/admin/categories')
+    }
+
+    Category.findByPk(id).then( category => {
+        if(category != undefined) {
+            res.render('admin/categories/edit', {category: category})
+        } else {
+            res.redirect('/admin/categories')
+        }
+    }).catch( err => {
+        res.redirect('/admin/categories')
+    })
+})
+
 module.exports = router
