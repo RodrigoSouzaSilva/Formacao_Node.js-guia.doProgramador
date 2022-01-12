863171dcf96751679799ebdd3dc7674b7c20e92c
RodrigoSouzaSilva <rodrigo_souza.91@hotmail.com>
2022-01-11 22:25:23 -0300

    atualizando projeto e anotações 07

--- 07_Projeto#02_Criando_um_blog_com_painel_administrativo/guiaPress/articles/ArticlesController.js
+++ 07_Projeto#02_Criando_um_blog_com_painel_administrativo/guiaPress/articles/ArticlesController.js
@@ -94,15 +94,24 @@ router.get('/articles/page/:num', (req, res) => {
     if(isNaN(page) || page == 0) {
         offset = 0
     } else {
-        offset = parseInt(page) * 4
+        offset = (parseInt(page) - 1) * 4
     }
 
     Article.findAndCountAll({
-        limit: 4,
-        offset: offset
-    }).then(articles => {
 
+        // quando se trabalha com o findAndCountAll
+        // ele retorna 2 coisa
+        // count -> a quantidade de elemento que tem
+        // rows  -> as listas de artigos
+
+        limit: 4,
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
         let next
+
         if(offset + 4 >= articles.count){
             next = false
         } else {
@@ -110,11 +119,14 @@ router.get('/articles/page/:num', (req, res) => {
         }
 
         let result = {
+            page: parseInt(page),
             next: next,
             articles: articles
         }
 
-        res.json(result)
+        Category.findAll().then(categories => {
+            res.render('admin/articles/page', {result: result, categories: categories})
+        })
     })
 })
 
